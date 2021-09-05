73f43f7a3a56188b1a90f1fa7cd4c7680f91fc18
B.DHEERAJ CHANDAN <88262353+BDheerajChandan@users.noreply.github.com>
2021-09-05 10:17:57 +0530

    Add files via upload
    Write a program in java to print a name

---
+++ Print_Name1.java
@@ -0,0 +1,11 @@
+/*Write a program in java to print a name */
+import java.io.*;
+public class Print_Name1 {
+
+	public static void main(String[] args) {
+		System.out.println("B Dheeraj Chandan");
+		// TODO Auto-generated method stub
+
+	}
+
+}
